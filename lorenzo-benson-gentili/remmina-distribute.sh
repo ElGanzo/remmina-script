5ae24ce95297f15ba95b37ca69118ab17aea622d
ElGanzo <lorenzobensong@gmail.com>
2018-01-04 18:17:34 +0100

    Aggiunta commenti e correzione script
    Ho aggiunto dei commenti per aiutare la comprensione dello script, ho corretto gli errori ( eliminando i commenti nei 'sed' ) e ho cercato di rendere migliore la grafica dello script eliminando alcuni spazi inutili

--- lorenzo-benson-gentili/remmina-distribute.sh
+++ lorenzo-benson-gentili/remmina-distribute.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-utenti="$(ls /home/)"
-TUTTI_GLI_IP="10.2.60.100 10.2.4.140 10.2.4.11 10.2.7.100 10.2.40.100"
+utenti="$(ls /home/)"		# per ogni utente	# command substitution
+TUTTI_GLI_IP="10.2.60.100 10.2.4.140 10.2.4.11 10.2.7.100 10.2.40.100"		# per ogni IP
 
-for user in $utenti
+for user in $utenti		# variabile d'appoggio  # variable substitution
 do
-
 	for IP in $TUTTI_GLI_IP
 	do
-		# cp remoto.sh /home/$user/Desktop/remoto-$IP.sh
-		# sed "s/VM.remmina/VM-$IP.remmina/g" remoto.sh > /home/$user/Desktop/remoto-$IP.sh
-		# sed "s/10.2.60.100/$IP/g" VM.remmina > VM-$IP.remmina
-
+		sed "s/VM.remmina/VM-$IP.remmina/g" remoto.sh > /home/$user/Desktop/remoto-$IP.sh
+		sed "s/10.2.60.100/$IP/g" VM.remmina > VM-$IP.remmina
 		cp VM-$IP.remmina /home/$user/Desktop/ 
 	done
 done
